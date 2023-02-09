6fa95e93148b3c182a7c2a1218d55c91bf91ec3a
Nabaa Busbaih <nabaajafar3@gmail.com>
2023-02-10 00:52:36 +0300

    -

---
+++ Cinema/Cinema/test/ProjectTest.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import com.mysql.jdbc.Connection;
+import static org.junit.Assert.*;
+import cinema.MyFrame;
+public class ProjectTest {
+    
+    public ProjectTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+ @Test
+ public void testOpenConnection()throws Exception{
+     System.out.println("OpenConnection");
+     java.sql.Connection result=MyFrame.OpenConnection();
+             assertEquals(result!=null,true);
+ }
+}
